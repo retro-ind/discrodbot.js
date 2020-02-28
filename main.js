4cdb76232c0f04912dee89d37da3e624e89276d5
Hash-Bomb <61591949+Hash-Bomb@users.noreply.github.com>
2020-02-28 10:53:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,19 +5,18 @@ client.on('ready', () => {
 });
 var prefix = "r-";
 client.on('message', msg => {  
+    
     if(msg.author.bot) return;
     if (msg.content.toLowerCase() === prefix+'ping') {     
-        msg.reply('Pong!');   
-    if(msg.author.bot) return;
+        msg.reply('Pong!')};   
     if (msg.content.toLowerCase() === prefix) {     
-        msg.reply('Specify Command or r-help for command list');   
-    if(msg.author.bot) return;
+        msg.reply('Specify Command or r-help for command list')};   
     if (msg.content.toLowerCase() === prefix+'help') {     
         msg.reply('
                   'Command list:'
                   'r-help : This List!'
                   'r-ping : Pong!'
-                  ');   
+                  ')};   
     } 
 });
 
