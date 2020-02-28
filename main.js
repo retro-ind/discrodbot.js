d359854179ef96b2c0b6a33f33e4551a431396d9
Hash-Bomb <61591949+Hash-Bomb@users.noreply.github.com>
2020-02-28 10:28:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.on('ready', () => {   
     console.log(`Logged in as ${client.user.tag}!`); 
 });
-var prefix = "r-";
+var prefix = "r-","R-";
 client.on('message', msg => {  
     if(msg.author.bot) return;
     if (msg.content === prefix+'ping') {     
