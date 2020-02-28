f927cc8b4d82449c50f85c7a75f099649574715d
Hash-Bomb <61591949+Hash-Bomb@users.noreply.github.com>
2020-02-28 09:39:29 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.on('ready', () => {   
     console.log(`Logged in as ${client.user.tag}!`); 
 });
-client.on('r-', msg => {  
+client.on('message', msg => {  
     if(message.author.bot) return;
     if (msg.content === 'ping') {     
         msg.reply('Pong!');   
