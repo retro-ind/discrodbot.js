5477716143c907837c29c9a37a3ae4bc07c824ed
Hash-Bomb <61591949+Hash-Bomb@users.noreply.github.com>
2020-03-02 07:37:39 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ client.on('message', msg => {
         msg.channel.send('Specify Command or r-help for command list');
     }
     if (msg.content.toLowerCase() === prefix+'help') {     
-        msg.channel.send('\nCommandlist:\n```r-help : This List!\nr-ping : Pong! \nr-kill (mention user) : kills the mentiond user \nr-joke : jokes \nr-v : version \nr-about : info about R.O.B.```');
+        msg.channel.send('\nCommandlist:\n```r-help : This List!\nr-ping : Pong! \nr-kill (mention user) : kills the mentiond user \nr-joke : jokes \nr-v : version```');
     }
     if (msg.content.toLowerCase().startsWith(prefix+'kill')) {
         if (msg.mentions.users.first() !== undefined ) {
@@ -27,7 +27,7 @@ client.on('message', msg => {
             msg.channel.send(msg.content.substring(args[0].length+1, msg.content.length)+' was killed mercilessly');   
         }   
    }
-   var array = ['Your Life','Not right now','Вы шутка, лол','***BEAN***'];
+    var array = ['Your Life','Not right now','Вы шутка, лол','***BEAN***'];
     function choose(choices) {
     var index = Math.floor(Math.random() * choices.length);
     return choices[index];
@@ -41,13 +41,12 @@ client.on('message', msg => {
     if (msg.content.toLowerCase() === 'cringe') {     
         msg.channel.send('***WARNING CRINGE NORMIE DETECTED*** \n ***DESTROY CRINGE NORMIE? YES, NO?***');{
     }
-      if (msg.content.toLowerCase() === (prefix+'yes')) {
+    if (msg.content.toLowerCase() === (prefix+'yes')) {
         msg.channel.send('***DESTROYING CRINGE NORMIE***');
-      } else {
-      msg.channel.send('***CRINGE NORMIE DESTRUCTION CANCELED***');
-      }
-    if (msg.content.toLowerCase() === prefix+'about') {     
-        msg.channel.send('R.O.B. (Robotic Operating Buddy) is a toy robot accessory for the Nintendo Entertainment System (NES). It was launched in July 1985 as the Family Computer Robot[a] in Japan, and October 1985 as R.O.B. in North America. Its short lifespan yielded only two games in the Robot Series: Gyromite and Stack-Up. Following the devastating North American video game crash of 1983, Nintendo alleviated the fearful retail market by rebranding its Japanese Famicom video game console as the Nintendo Entertainment System—a new platform focused on R.O.B. to further reclassify the system as a uniquely sophisticated toy experience instead of simply as a video game console.[1][2] Computer Entertainer magazine in June 1985 called R.O.B. "the world's only interactive robot".[3] The extensive marketing plan centered on R.O.B., which began at the October 1985 test market launch of the NES, was ultimately successful. This launch represented Nintendo's debut in the North American video game console market, and yielded the revitalization of the entire video game industry. Although R.O.B. was quietly discontinued a few years later, it is nevertheless remembered as a successful Trojan Horse of marketing, and has even gained renewed attention as a playable character within the Super Smash Bros. series.');
+    } else {
+    msg.channel.send('***CRINGE NORMIE DESTRUCTION CANCELED***');
+    }
+    
     }
 });
 
